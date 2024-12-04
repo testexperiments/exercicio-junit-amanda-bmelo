368ddacaf444282de0f36b215ed3c204fb742878
Amanda <ammelo@id.uff.br>
2024-12-04 11:59:22 +0000

    novos testes

--- test/calculadora/CalculadoraTest.java
+++ test/calculadora/CalculadoraTest.java
@@ -1,6 +1,6 @@
 package calculadora;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
@@ -10,24 +10,40 @@ public class CalculadoraTest {
     @Test
     public void testSoma() {
         double soma = c.calc(2.0, 9.0, '+');
-        assertEquals(11.0, soma, 0.0);
+        Assertions.assertEquals(11.0, soma, 0.0);
     }
 
     @Test
     public void testSubtracao() {
         double subtracao = c.calc(9.0, 9.0, '-');
-        assertEquals(0.0, subtracao, 0.0);
+        Assertions.assertEquals(0.0, subtracao, 0.0);
     }
 
     @Test
     public void testMultiplicacao() {
         double multiplicacao = c.calc(9.0, 2.0, '*');
-        assertEquals(18.0, multiplicacao, 0.0);
+        Assertions.assertEquals(18.0, multiplicacao, 0.0);
     }
 
     @Test
     public void testDivisao() {
         double divisao = c.calc(9.0, 2.0, '/');
-        assertEquals(4.5, divisao, 0.0);
+        Assertions.assertEquals(4.5, divisao, 0.0);
+    }
+
+    @Test
+    public void testPrimitiveReturn() {
+        double multiplicacao = c.calc(9.0, 0.0, '*');
+        Assertions.assertEquals(0.0, multiplicacao, 0.0);
+        // testar tipo retornado
+    }
+
+    @Test
+    public void testError() {
+        RuntimeException thrown = Assertions.assertThrowsExactly(RuntimeException.class, () -> {
+            c.calc(9.0, 2.0, '=');
+            throw new IllegalArgumentException("Invalid operation");
+        });
+        Assertions.assertEquals("Invalid operation", thrown.getMessage());
     }
 }
